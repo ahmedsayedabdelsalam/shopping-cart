5c66436bd81261ff9f59edb27fa645f79e7ab491
ahmedsayedll <e.eng.ahmedsayed@gmail.com>
2018-05-27 15:17:18 +0200

    final touches

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -8,6 +8,9 @@ use App\OrderApi;
 use Auth;
 use App\Order;
 use App\Cart;
+use Stripe\Stripe;
+use Stripe\Token;
+use Stripe\Charge;
 
 class OrderController extends Controller
 {
@@ -58,35 +61,77 @@ class OrderController extends Controller
     }
 
     public function checkout(Request $request) {
-        
-        $orders = OrderApi::where('user_id', auth()->user()->id)->with('product')->get();
-
-        $cart = new Cart(null);
-
-        foreach($orders as $order) {
-            $cart->items[$order->product->id]['item'] = $order->product;
-            $cart->items[$order->product->id]['qty'] = $order->count;
-            $cart->items[$order->product->id]['price'] = $order->product->price * $order->count;
-            $cart->totalQty += $cart->items[$order->product->id]['qty'];
-            $cart->totalPrice += $cart->items[$order->product->id]['price'];
-        }
-
+        // validate request data
         $this->validate($request, [
             'name' => 'required',
             'address' => 'required',
-            'payment_id' => 'required'
+            'card_exp_month' => 'required',
+            'card_exp_year' => 'required',
+            'card_number' => 'required',
+            'card_cvc' => 'required'
         ]);
-        
-        $order = new Order([
-            'name' => $request->input('name'),
-            'address' => $request->input('address'),
-            'payment_id' => $request->input('payment_id'),
-            'user_id' => auth()->user()->id,
-            'cart' => serialize($cart)
-        ]);
-        $order->save();
 
-        return response()->json(['message' => 'items purchased successfully'], 200);
+        // create stripe api
+        Stripe::setApiKey("sk_test_P21lV4cw4w0ShOpjQTq3wkF0");
+
+        // get card data from user
+        $result = Token::create(
+            array(
+                "card" => array(
+                    "name" => $request['name'],
+                    "number" => $request['card_number'],
+                    "exp_month" => $request['card_exp_month'],
+                    "exp_year" => $request['card_exp_year'],
+                    "cvc" => $request['card_cvc']
+                )
+            )
+        );
+
+        // generate token if card data is valid
+        $token = $result['id'];
+
+        // get orders from orders-api table instead of session 
+        $orders = OrderApi::where('user_id', auth()->user()->id)->with('product')->get();
+
+        if($orders->count()) {
+            // make new cart object
+            $cart = new Cart(null);
+
+            // get data from orders-api table and put it into cart object
+            foreach($orders as $order) {
+                $cart->items[$order->product->id]['item'] = $order->product;
+                $cart->items[$order->product->id]['qty'] = $order->count;
+                $cart->items[$order->product->id]['price'] = $order->product->price * $order->count;
+                $cart->totalQty += $cart->items[$order->product->id]['qty'];
+                $cart->totalPrice += $cart->items[$order->product->id]['price'];
+            }
+
+            // make the charge
+            $charge = Charge::create(array(
+                "amount" => $cart->totalPrice * 100,
+                "currency" => "usd",
+                "card" => $token,
+                "description" => "Charge for test@example.com" 
+            ));
+
+
+            // make order
+            $order = new Order([
+                'name' => $request->input('name'),
+                'address' => $request->input('address'),
+                'payment_id' => $charge->id,
+                'user_id' => auth()->user()->id,
+                'cart' => serialize($cart)
+            ]);
+            $order->save();
+
+            $orders->each->delete();
+
+            return response()->json(['message' => 'items purchased successfully'], 200);
+        } else {
+            return response()->json(['message' => 'put items to cart first'], 200);
+        }
+        
     }
         
 }
